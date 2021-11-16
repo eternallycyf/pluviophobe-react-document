e30a01e757d8cc0c0514833f8b10355971b60180
Eternallycyf <969475322@qq.com>
2021-11-16 17:05:33 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 删除旧的文件
 rm -rf docs/.rcpress/dist
-# 生成静态文件
-# npm run build
-npm run generate
+
+# 生成新的压缩文件
+npm run build
 
 # 进入生成的文件夹
-
 cd docs/.rcpress/dist
+
+# 复制一个 index.html并命名为 404.html 解决 historyAPI 404问题
+cp index.html 404.html
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
